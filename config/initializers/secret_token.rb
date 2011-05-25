95967912b72fba806455545abffc1a9285109724
Keith Mc Donnell <keith@dancingtext.com>
2011-05-25 13:49:04 +0100

    Full screen map of all Rubyists in the UK

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UkrbMap::Application.config.secret_token = 'c4d65e0ae574051acbb31aea5f681b59c2892a43a1b305bb7ba5512dd30667037602aa1fe80b2b24f021252a4071a5f4ed78966be8695e4946df14a23f9c19b6'
