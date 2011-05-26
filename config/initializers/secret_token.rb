bca60c503edfc943f019596e1fcea631cbc4e891
Keith Mc Donnell <keith@dancingtext.com>
2011-05-26 17:42:33 +0100

    Basic setup for Google map

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-UkrbMap::Application.config.secret_token = 'c4d65e0ae574051acbb31aea5f681b59c2892a43a1b305bb7ba5512dd30667037602aa1fe80b2b24f021252a4071a5f4ed78966be8695e4946df14a23f9c19b6'
+UkrbMap::Application.config.secret_token = '888f2d513e9bfc57e58aad979919d5ae037e5f1216aedfc7ba871e8d8835db817dead4913f22f7e672ff2532a07a8a877c1244b8f86bd3b6cdf5992c6a587933'
